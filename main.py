ba928d2b8ec94a8112181aae308656de53d8e973
Andrés Court <andres@andrescourt.com>
2022-09-11 10:38:09 -0500

    Drawing polygons starting with a triangle and working all the way up to a 10 sided polygon

--- main.py
+++ main.py
@@ -10,12 +10,19 @@ tim.pencolor("PaleGreen4")
 #    tim.fd(side_size)
 #    tim.rt(90)
 
-dash_size = 5
-for _ in range(50):
-    tim.pd()
-    tim.fd(dash_size)
-    tim.pu()
-    tim.fd(dash_size)
+# dash_size = 10
+# for _ in range(15):
+#     tim.pd()
+#     tim.fd(dash_size)
+#     tim.pu()
+#     tim.fd(dash_size)
+
+line_length = 100
+for sides in range(3, 11):
+    angle = 360 / sides
+    for _ in range(sides):
+        tim.fd(line_length)
+        tim.rt(angle)
 
 # Has to happen after all the code
 screen = Screen()
