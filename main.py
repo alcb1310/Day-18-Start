9b8df6539b15992ce0b264dd78720cc57ee47e01
Andrés Court <andres@andrescourt.com>
2022-09-11 10:08:13 -0500

    Drawing a square with the turtle, fd -> moves the turtle forward and draws a line, rt -> turns the turtle right in the specified angle

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.fillcolor("PaleGreen4")
-timmy_the_turtle.pencolor("PaleGreen4")
-timmy_the_turtle.fd(100)
-timmy_the_turtle.rt(90)
+side_size = 50
+tim = Turtle()
+tim.shape("turtle")
+tim.fillcolor("PaleGreen4")
+tim.pencolor("PaleGreen4")
+
+for _ in range(4):
+    tim.fd(side_size)
+    tim.rt(90)
 
 # Has to happen after all the code
 screen = Screen()
