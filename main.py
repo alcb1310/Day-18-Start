a5d70d257916a368c6f715d01510e33a65fb1851
Andrés Court <andres@andrescourt.com>
2022-09-11 10:55:09 -0500

    Drawing polygons with random colors each time

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from turtle import Turtle, Screen
+from random import randint
 
 tim = Turtle()
+screen = Screen()
+
 tim.shape("turtle")
 tim.fillcolor("PaleGreen4")
 tim.pencolor("PaleGreen4")
@@ -16,14 +19,17 @@ tim.pencolor("PaleGreen4")
 #     tim.fd(dash_size)
 #     tim.pu()
 #     tim.fd(dash_size)
-
+screen.colormode(255)
 line_length = 100
 for sides in range(3, 11):
     angle = 360 / sides
+    r = randint(0, 254)
+    g = randint(0, 254)
+    b = randint(0, 254)
+    tim.pencolor(r, g, b)
     for _ in range(sides):
         tim.fd(line_length)
         tim.rt(angle)
 
 # Has to happen after all the code
-screen = Screen()
 screen.exitonclick()
