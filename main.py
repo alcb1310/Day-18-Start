ea5cfc8bb82c1515ac56206ed6859cf9197cf6b2
Andrés Court <andres@andrescourt.com>
2022-09-11 10:28:57 -0500

    Draw a dotted line

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from turtle import Turtle, Screen
 
-side_size = 50
 tim = Turtle()
 tim.shape("turtle")
 tim.fillcolor("PaleGreen4")
 tim.pencolor("PaleGreen4")
 
-for _ in range(4):
-    tim.fd(side_size)
-    tim.rt(90)
+# or _ in range(4):
+#    side_size = 50
+#    tim.fd(side_size)
+#    tim.rt(90)
+
+dash_size = 5
+for _ in range(50):
+    tim.pd()
+    tim.fd(dash_size)
+    tim.pu()
+    tim.fd(dash_size)
 
 # Has to happen after all the code
 screen = Screen()
